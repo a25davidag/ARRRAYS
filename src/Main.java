717fc5b0afbf868792cc728d3108a754f33502be
David Arranz González <a25davidag@teis.intranet>
2025-09-29 19:52:47 +0200

    clase

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,27 @@ public class Main {
                 System.out.println(temperaturas[i] + " grados");
             }
         }*/
-
-    //Random streamer = new Random();
+    //Ejercicio 2
     int numRamdon = (int) (Math.random()*100) + 1;
     int[] viwers = new int[200];
+    int[] mayores = new int[200];
+    int contador = 0;
 
      for (int x = 0; x <= 200-1; x++){
-         viwers[x] = (int) (Math.random()*1000) + 1;
+         viwers[x] = (int) (Math.random()*1000) + 800;
          System.out.println(viwers[x]);
          if (viwers[x] > 1000){
+             mayores[contador] = viwers[x];
+             contador++;
              System.out.println("Ha superado las visitas");
          }else{
              System.out.println("no ha superado");
          }
         }
-     }
+        System.out.println("Han superado las visitas");
+     }else{
+
+    }
     }
 
 
